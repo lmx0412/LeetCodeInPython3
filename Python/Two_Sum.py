395ee4e858edac8000e66a8b2c1357e5b7c6fdc1
liumengxuan <liumengxuan@mobike.com>
2018-08-29 00:35:47 +0800

    Two_Sum solution added

--- Python/Two_Sum.py
+++ Python/Two_Sum.py
@@ -11,3 +11,34 @@ examples:
 """
 
 
+nums = [2, 7, 11, 15]
+target = 9
+nums = [3, 2, 4]
+target = 6
+nums = [3, 2, 3]
+target = 6
+nums = [3, 3]
+target = 6
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            tmp = nums.copy()
+            tmp.pop(i)
+            if (target - nums[i]) in tmp:
+                return [i, tmp.index(target - nums[i]) + 1]
+        return [0, 0]
+
+
+def main():
+    solution = Solution()
+    ret = solution.twoSum(nums, target)
+    print(ret)
+
+if __name__ == "__main__":
+    main()
